d29560f329596c78b6e4321821580c081358e5ff
malisaroj <malisaroj@icloud.com>
2020-05-22 15:37:19 +0545

    add cart

---
+++ src/components/cart.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+import { Store } from "../store/Store";
+export default {
+  computed: {
+    cart() {
+      return Store.$data.cart;
+    }
+  },
+  methods: {
+    removeFromCart(sku) {
+      Store.removeFromCart(sku);
+    },
+    addToCart(product) {
+      Store.addToCart(product);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/components/category.vue
+++ src/components/category.vue
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import { Store } from "../store/Store";
 export default {
   data() {
     return {
@@ -29,36 +30,9 @@ export default {
   },
   props: ["productInfo"],
 
-  computed: {
-    totalCost() {
-      return this.cart.reduce((accum, product) => {
-        return accum + product.details.price * product.quantity;
-      }, 0);
-    }
-  },
   methods: {
     addToCart(product) {
-      const locationInCart = this.cart.findIndex(p => {
-        return p.details.sku === product.sku;
-      });
-      if (locationInCart === -1) {
-        this.cart.push({
-          details: product,
-          quantity: 1
-        });
-      } else {
-        this.cart[locationInCart].quantity++;
-      }
-    },
-    removeFromCart(sku) {
-      const locationInCart = this.cart.findIndex(p => {
-        return p.details.sku === sku;
-      });
-      if (this.cart[locationInCart].quantity <= 1) {
-        this.cart.splice(locationInCart, 1);
-      } else {
-        this.cart[locationInCart].quantity--;
-      }
+      Store.addToCart(product);
     }
   }
 };
